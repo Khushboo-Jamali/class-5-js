c47c5e175d213504badde46839f2626074fa2465
khushboo-jamali <jamalikhushboo@gmail.com>
2024-02-19 22:51:30 +0100

    start

---
+++ app.js
@@ -0,0 +1,7 @@
+let a = prompt("Enter the table");
+
+// let a = 2;
+for (let table = 1; table <= 10; table++) {
+  document.write(a + " x " + table + " = " + a * table + "<br>");
+  console.log(a + " x " + table + " = " + a * table);
+}
